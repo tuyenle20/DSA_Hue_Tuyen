7e5118f3384fffcea8ecf424be054adf2b3e1afd
tuyenle20 <lethingoctuyen10ta1@gmail.com>
2025-06-04 23:31:48 +0700

    Update Main.java

--- Main/Main.java
+++ Main/Main.java
@@ -10,10 +10,9 @@ public class Main {
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("2D Adventure");
-        //window.setUndecorated(true);
 
-        GamePanel gamePanel = new GamePanel(); // Corrected "GamePanel" to lowercase "gamePanel" as it's a variable
-        window.add(gamePanel); // Fixed the reference to the `GamePanel` instance
+        GamePanel gamePanel = new GamePanel(); 
+        window.add(gamePanel); 
 
         window.pack();
         window.setLocationRelativeTo(null);
